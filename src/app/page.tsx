44d44f2a606a41617c576149a8d4b6face9d27ea
Amaan63 <amaansayed63@gmail.com>
2025-10-21 14:27:07 +0530

    Added experience section in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import AboutSection from "@/sections/AboutUs";
+import ExperienceSection from "@/sections/ExperienceSection";
 import HeroSection from "@/sections/HeroSection";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <main>
       <HeroSection />
       <AboutSection />
+      <ExperienceSection />
     </main>
   );
 }
