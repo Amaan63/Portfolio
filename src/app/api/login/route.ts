b69b5f84658ae0e60ffd1215e947e806bc0f2c4a
Amaan63 <amaansayed63@gmail.com>
2025-11-07 22:30:11 +0530

    Created Login api

---
+++ src/app/api/login/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const scope = [
+    "user-top-read", // required to access top tracks
+  ].join(" ");
+
+  const params = new URLSearchParams({
+    client_id: process.env.SPOTIFY_CLIENT_ID!,
+    response_type: "code",
+    redirect_uri: process.env.SPOTIFY_REDIRECT_URI!,
+    scope,
+  });
+
+  return NextResponse.redirect(
+    `https://accounts.spotify.com/authorize?${params.toString()}`
+  );
+}
