2290479cb80de4776fc888d13a92d6dc0a6b1721
Amaan63 <amaansayed63@gmail.com>
2025-11-21 20:48:27 +0530

    added infinite scroll

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,17 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "infinite-scroll": "infinite-scroll 25s linear infinite",
+      },
+      keyframes: {
+        "infinite-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
+      },
+    },
   },
   plugins: [],
 };
